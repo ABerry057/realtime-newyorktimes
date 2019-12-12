1bee80793b56867c004038c1e34cd572c1850344
Alexander Berry <alexander_berry@brown.edu>
2019-12-12 13:38:28 +0000

    cosmetic changes

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def description():
     Returns overall project description in markdown
     """
     return html.Div(children=[dcc.Markdown('''
-        ### The News. Visualized.
-        # Lorem ipsum
+        # The News. Visualized.
+        ### Lorem ipsum
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 
