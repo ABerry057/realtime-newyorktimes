06f48a495523e22b7a8bfb4df291ef97d2b1dee4
Alexander Berry <alexander_berry@brown.edu>
2019-12-12 14:20:46 +0000

    [Fix] better clear_data.py

--- clear_data.py
+++ clear_data.py
@@ -7,5 +7,10 @@ if __name__ == "__main__":
     client = MongoClient("mongodb+srv://team:dummyPassword@cluster0-6vgfj.mongodb.net/test?retryWrites=true&w=majority")
 
     db = client.new_york_times
+    db.articles.drop()
 
+    db = client.test
+    db.articles.drop()
+
+    db = client.test_db
     db.articles.drop()
